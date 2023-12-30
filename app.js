61c51a35bd8cba514c1db4f546f3a77c00bd424d
Chris Smart <Chris@Chriss-MacBook-Pro.local>
2023-12-30 20:59:29 +0000

    begin simple picker

---
+++ app.js
@@ -0,0 +1,15 @@
+const colours = ["green", "red", "rgba(92,133,6)", "#f415f6"];
+const btn = document.getElementById("btn");
+const colour = document.querySelector(".colour");
+
+btn.addEventListener("click", function () {
+  // get random number
+  const randomNumber = getRandomNum();
+  console.log(randomNumber);
+  document.body.style.backgroundColor = colours[randomNumber];
+  colour.textContent = colours[randomNumber];
+});
+
+function getRandomNum() {
+  return Math.floor(Math.random() * colours.length);
+}
